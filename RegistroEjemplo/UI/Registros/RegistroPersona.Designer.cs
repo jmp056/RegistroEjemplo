3723ed5bc85207a422cc1db0700272ecc54e7c8f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-14 22:16:17 -0400

    Terminando la programacion de la ventana registro

---
+++ RegistroEjemplo/UI/Registros/RegistroPersona.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace RegistroEjemplo.UI.Registros
+{
+    partial class RegistroPersona
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CedulamaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.DirecciontextBox = new System.Windows.Forms.TextBox();
+            this.IDnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.MyErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.IDnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Location = new System.Drawing.Point(631, 89);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Buscarbutton.TabIndex = 0;
+            this.Buscarbutton.Text = "button1";
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            this.Buscarbutton.Click += new System.EventHandler(this.Buscarbutton_Click);
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.Location = new System.Drawing.Point(620, 131);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(75, 23);
+            this.Nuevobutton.TabIndex = 1;
+            this.Nuevobutton.Text = "button2";
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Location = new System.Drawing.Point(596, 184);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Guardarbutton.TabIndex = 2;
+            this.Guardarbutton.Text = "button3";
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Location = new System.Drawing.Point(596, 238);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Eliminarbutton.TabIndex = 3;
+            this.Eliminarbutton.Text = "button4";
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            this.Eliminarbutton.Click += new System.EventHandler(this.Eliminarbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(190, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(190, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // CedulamaskedTextBox
+            // 
+            this.CedulamaskedTextBox.Location = new System.Drawing.Point(288, 131);
+            this.CedulamaskedTextBox.Mask = "999-9999999-9";
+            this.CedulamaskedTextBox.Name = "CedulamaskedTextBox";
+            this.CedulamaskedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CedulamaskedTextBox.TabIndex = 8;
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(288, 82);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(100, 20);
+            this.NombretextBox.TabIndex = 10;
+            // 
+            // DirecciontextBox
+            // 
+            this.DirecciontextBox.Location = new System.Drawing.Point(246, 167);
+            this.DirecciontextBox.Name = "DirecciontextBox";
+            this.DirecciontextBox.Size = new System.Drawing.Size(100, 20);
+            this.DirecciontextBox.TabIndex = 11;
+            // 
+            // IDnumericUpDown
+            // 
+            this.IDnumericUpDown.Location = new System.Drawing.Point(288, 49);
+            this.IDnumericUpDown.Name = "IDnumericUpDown";
+            this.IDnumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.IDnumericUpDown.TabIndex = 12;
+            // 
+            // MyErrorProvider
+            // 
+            this.MyErrorProvider.ContainerControl = this;
+            // 
+            // RegistroPersona
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.IDnumericUpDown);
+            this.Controls.Add(this.DirecciontextBox);
+            this.Controls.Add(this.NombretextBox);
+            this.Controls.Add(this.CedulamaskedTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.Buscarbutton);
+            this.Name = "RegistroPersona";
+            this.Text = "RegistroPersona";
+            ((System.ComponentModel.ISupportInitialize)(this.IDnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox CedulamaskedTextBox;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.TextBox DirecciontextBox;
+        private System.Windows.Forms.NumericUpDown IDnumericUpDown;
+        private System.Windows.Forms.ErrorProvider MyErrorProvider;
+    }
+}
---
+++ RegistroEjemplo/UI/Registros/RegistroPersona.cs
@@ -0,0 +1,153 @@
+﻿using RegistroEjemplo.BLL;
+using RegistroEjemplo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroEjemplo.UI.Registros
+{
+    public partial class RegistroPersona : Form
+    {
+        public RegistroPersona()
+        {
+            InitializeComponent();
+        }
+
+        private void Limpiar()
+        {
+            IDnumericUpDown.Value = 0;
+            NombretextBox.Text = string.Empty;
+            DirecciontextBox.Text = string.Empty;
+            DirecciontextBox.Text = string.Empty;
+            MyErrorProvider.Clear();
+        }
+
+        private void Nuevobutton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private Personas LlenaCLase()
+        {
+            Personas persona = new Personas();
+            persona.PersonaId = Convert.ToInt32(IDnumericUpDown.Value);
+            persona.Nombre = NombretextBox.Text;
+            persona.Cedula = DirecciontextBox.Text;
+            persona.Direccion = DirecciontextBox.Text;
+
+            return persona;
+        }
+
+        private void LlenaCampo(Personas persona)
+        {
+            IDnumericUpDown.Value = persona.PersonaId;
+            NombretextBox.Text = persona.Nombre;
+            CedulamaskedTextBox.Text = persona.Cedula;
+            DirecciontextBox.Text = persona.Direccion;
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Personas persona = PersonasBLL.Buscar((int)IDnumericUpDown.Value);
+            return (persona != null);
+        }
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            Personas persona;
+            bool paso = false;
+
+            if (!Validar())
+                return;
+
+            persona = LlenaCLase();
+            Limpiar();
+
+            if (IDnumericUpDown.Value == 0)
+                paso = PersonasBLL.Guardar(persona);
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+                    MessageBox.Show("No se puede modificar una persona que no existe", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                paso = PersonasBLL.Modificar(persona);
+            }
+
+            if (paso)
+                MessageBox.Show("Guardado!", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("No fue posible guardar!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+            MyErrorProvider.Clear();
+
+            if (NombretextBox.Text == string.Empty)
+            {
+                MyErrorProvider.SetError(NombretextBox, "El campo nombre no puede estar vacio");
+                NombretextBox.Focus();
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(DirecciontextBox.Text))
+            {
+                MyErrorProvider.SetError(DirecciontextBox, "El campo Direccion no puede estar vacio");
+                DirecciontextBox.Focus();
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(CedulamaskedTextBox.Text.Replace("-", "")))
+            {
+                MyErrorProvider.SetError(CedulamaskedTextBox, "El campo cedula no puede estar vacio");
+                CedulamaskedTextBox.Focus();
+                paso = false;
+            }
+            return paso;
+        }
+
+        private void Buscarbutton_Click(object sender, EventArgs e)
+        {
+            int id;
+            Personas persona = new Personas();
+            int.TryParse(IDnumericUpDown.Text, out id);
+
+            Limpiar();
+
+            persona = PersonasBLL.Buscar(id);
+
+            if (persona != null)
+            {
+                MessageBox.Show("Persona Encontrada");
+                LlenaCampo(persona);
+            }
+            else
+            {
+                MessageBox.Show("Persona no encontrada");
+            }
+        }
+
+        private void Eliminarbutton_Click(object sender, EventArgs e)
+        {
+            MyErrorProvider.Clear();
+
+            int id;
+            int.TryParse(IDnumericUpDown.Text, out id);
+
+            Limpiar();
+
+            if (PersonasBLL.Eliminar(id))
+                MessageBox.Show("Eliminado", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MyErrorProvider.SetError(IDnumericUpDown, "No se puede eliminar una persona que no existe");
+        }
+    }
+}
